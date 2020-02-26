fc76d7cb95994628f2ff183d030c3fdef0b6a214
Martijn Rondeel <martijn@rondeel.email>
2020-02-26 17:49:01 +0100

    Remove source-map-support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 // Compile our .ts files with ts-node so Gatsby's NodeJS side can handle them
-require('source-map-support').install();
 require('ts-node').register({ files: true });
 
 module.exports = require('./gatsby-config.ts');
