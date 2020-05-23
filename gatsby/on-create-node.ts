3f07a5580d8168849eda31f071fd95f12890c129
Martijn Rondeel <martijn@rondeel.email>
2020-05-23 19:02:25 +0200

    Remove private repositories from github graphql query

--- gatsby/on-create-node.ts
+++ gatsby/on-create-node.ts
@@ -1,11 +1,20 @@
 import { createFilePath } from 'gatsby-source-filesystem';
 import { GatsbyNode, Node } from 'gatsby';
-import { Post } from '../src/types';
+import { Post, AllMarkdownRemarkWithRepositories } from '../src/types';
 
 export const onCreateNode: GatsbyNode['onCreateNode'] = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
   const nodeCopy = (node as unknown) as Post;
+
+  // Remove any private repositories from our github graphql query result
+  if (node.internal.type === 'GithubData') {
+    const data = node.data as AllMarkdownRemarkWithRepositories['githubData']['data'];
+
+    data.user.topRepositories.nodes = data.user.topRepositories.nodes.filter(
+      (r) => !r.isPrivate,
+    );
+  }
 
   if (node.internal.type === 'MarkdownRemark') {
     if (typeof nodeCopy.frontmatter.slug !== 'undefined') {
